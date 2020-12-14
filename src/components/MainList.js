0469eccc1b2eb9fb41c62c891e5deb10fbbdefd8
김태연 <ty980407@gmail.com>
2020-12-14 21:52:33 +0900

    MainDetail로 이동하는 navigation 활용 위한 props 추가

--- src/components/MainList.js
+++ src/components/MainList.js
@@ -1,12 +1,10 @@
 import React, {useState} from 'react';
-import styled from 'styled-components';
-import {View, ScrollView, Text, Image} from 'react-native';
-import {SearchBar, Button} from 'react-native-elements';
+import {View, ScrollView, TouchableOpacity} from 'react-native';
 
 import sampleData from '../sampleData.json';
 import MainCard from './MainCard';
 
-const MainList = () => {
+const MainList = ({handleMainScreen}) => {
   const [data] = useState(sampleData);
   return (
     <ScrollView style={{width: '100%', marginTop: 10}}>
@@ -15,10 +13,16 @@ const MainList = () => {
           flexDirection: 'row',
           justifyContent: 'space-around',
           flexWrap: 'wrap',
+          marginBottom: 30,
         }}>
         {data.map((item) => {
-          // console.log(item.image_url);
-          return <MainCard key={item.id} data={item} />;
+          return (
+            <MainCard
+              key={item.id}
+              data={item}
+              handleMainScreen={handleMainScreen}
+            />
+          );
         })}
       </View>
     </ScrollView>
